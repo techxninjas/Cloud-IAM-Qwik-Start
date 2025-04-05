396c3a2289dd91ec6331020de183136a75386902
TechXNinjas <thetechxninjas@gmail.com>
2025-04-05 11:04:39 +0530

    Create TechXNinjas.sh

---
+++ TechXNinjas.sh
@@ -0,0 +1,45 @@
+# Define color codes for output formatting
+YELLOW_COLOR=$'\033[0;33m'
+NO_COLOR=$'\033[0m'
+BACKGROUND_RED=`tput setab 1`
+GREEN_TEXT=`tput setab 2`
+RED_TEXT=`tput setaf 1`
+
+BOLD_TEXT=`tput bold`
+RESET_FORMAT=`tput sgr0`
+
+echo "${BACKGROUND_RED}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+
+# Prompt user for their username
+read -p "${YELLOW_COLOR}Enter USERNAME 2: ${NO_COLOR}" USERNAME_2
+
+# Set the storage bucket and enable uniform bucket-level access
+gsutil mb -l us -b on gs://$DEVSHELL_PROJECT_ID
+
+# Create a sample file and write content to it
+echo "Subscribe to Arcade Crew" > sample.txt
+
+# Upload the sample file to the specified bucket
+gsutil cp sample.txt gs://$DEVSHELL_PROJECT_ID
+
+# Remove IAM policy binding for the specified user
+gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USERNAME_2 \
+  --role=roles/viewer
+
+# Add IAM policy binding for storage object viewer role
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USERNAME_2 \
+  --role=roles/storage.objectViewer
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
+
+echo "${GREEN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}              Lab Completed Successfully!               ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
+echo
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@TechXNinjas${RESET_FORMAT}"
+echo -e "${RED_TEXT}${BOLD_TEXT}Join our WhatsApp Group:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://chat.whatsapp.com/B7uNN97yIwRHQnlYux2Tmn${RESET_FORMAT}"
+echo -e "${RED_TEXT}${BOLD_TEXT}Follow me on LinkedIn:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.linkedin.com/in/iaadillatif/${RESET_FORMAT}"
+echo
